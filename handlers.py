3f8a2fd9df47c71862014ed1f91a94614c6a3a20
Андрей Исаков <andrew.isakov@gmail.com>
2020-04-01 13:22:34 +0300

    rest api base realisation

---
+++ handlers.py
@@ -0,0 +1,160 @@
+import uuid
+import ujson
+import decimal
+from aiohttp.web import json_response
+from models import User
+from schemas import schema_check
+
+
+async def ping(request):
+    return json_response(data={'message': 'pong'})
+
+
+async def get_user(user_id):
+    if user_id:
+        user_id = uuid.UUID(user_id)
+        user = await User.select('id', 'name', 'account', 'hold', 'status') \
+            .where(User.id == user_id).with_for_update().gino.one_or_none()
+    return user
+
+
+async def add(request):
+    data = await request.json()
+    user_id = data.get('id')
+    user = await get_user(user_id)
+    add_summ = data.get('sum', 0)
+    response = {}
+    if user and user.status:
+        new_account = user.account+decimal.Decimal(add_summ)
+        result = await User.update.values(account=new_account).where(User.id == user_id).gino.status()
+        result = result[0].split()
+        response = {
+            'result': result,
+            'description': 'Баланс {}пополнен.'.format('' if result[0] == 'UPDATE' and int(result[1]) else 'не '),
+        }
+        code = 200 if int(result[1]) else 401
+    elif user:
+        response = {
+            'result': 'NOT ALLOWED',
+            'description': 'Cant modify because user.status is closed.',
+        }
+        code = 400
+    else:
+        response = {
+            'addition': {'id': user_id},
+            'result': 'NOT FOUND',
+        }
+        code = 404
+    response.update(status=code)
+    if code != 404:
+        response.update(
+            addition={
+                'id': str(user.id),
+                'name': user.name,
+                'status': ('closed', 'open')[user.status],
+                'account': user.account,
+                'hold': user.hold,
+            },
+        )
+    response = ujson.dumps(response)
+    return json_response(body=response, status=code)
+
+
+async def substract(request):
+    data = await request.json()
+    user_id = data.get('id')
+
+    sub_summ = data.get('sum', 0)
+    user = await get_user(user_id)
+
+    if user and user.status:
+        sub_summ = decimal.Decimal(sub_summ)
+        new_hold = user.hold + sub_summ
+        holding_allow = user.account >= new_hold
+        if holding_allow:
+            
+            result = await User.update.values(hold=new_hold).where(User.id == user_id).gino.status()
+            result = result[0].split()
+            response = {
+                'result': result,
+                'description': 'Списание {}зарезервировано.'.format(
+                    '' if result[0] == 'UPDATE' and int(result[1]) else 'не '
+                ),
+            }
+            code = 200 if int(result[1]) else 415
+            user = await get_user(user_id)
+        else:
+            response = {
+                'result': 'Недостаточно средств!',
+                'description': 'Списание не выполнено.',
+            }
+            code = 415
+
+    elif user:
+        response = {
+            'result': 'NOT ALLOWED',
+            'description': 'Списание {}выполнено.'.format('' if result[0] == 'UPDATE' and int(result[1]) else 'не '),
+        }
+        code = 405
+    else:
+        response = {
+            'addition': {'id': user_id},
+            'result': 'NOT FOUND',
+        }
+        code = 404
+
+    response.update(status=code)
+    if code != 404:
+        response.update(
+            addition={
+                'id': str(user.id),
+                'name': user.name,
+                'status': ('closed', 'open')[user.status],
+                'account': user.account,
+                'hold': user.hold,
+            },
+        )
+    response = ujson.dumps(response)
+    return json_response(body=response, status=code)
+
+
+async def status(request):
+    user_id = request.match_info.get('id')
+
+    if user_id:
+        # user_id = uuid.UUID(user_id)
+        user = await get_user(user_id)
+        if user:
+            code = 200
+            response = {
+                'addition': {
+                    'status': ('closed', 'open')[user.status],
+                    'account': user.account,
+                    'name': user.name,
+                    'hold': user.hold,
+                    'id': str(user.id),
+                },
+                'result': 'success',
+                'status': code,
+            }
+        else:
+            code = 404
+            response = {
+                'addition': {
+                    'id': user_id,
+                },
+                'result': 'NOT FOUND',
+                'status': code,
+            }
+    else:
+        response = {'id': user_id, 'status': 401, 'result': 'error'}
+        code = 401
+    response = ujson.dumps(response)
+    return json_response(body=response, status=code)
+    
+
+def setup(app):
+    app.router.add_get('/api/status/{id}', status)
+    app.router.add_post('/api/add', add)
+    app.router.add_post('/api/substract', substract)
+    app.router.add_post('/api/ping', ping)
--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import asyncio
-from models import db, User, create_init
+from aiohttp import web
+from models import db, create_init
 from config import BIND
+from handlers import setup as route_setup
 
 
 async def main():
     await db.set_bind(BIND)
     await db.gino.create_all()
     await create_init()
-    await db.pop_bind().close()
-
+    # await db.pop_bind().close()
 
 asyncio.get_event_loop().run_until_complete(main())
+app = web.Application()
+route_setup(app)
+web.run_app(app, host='0.0.0.0', port=8080)
